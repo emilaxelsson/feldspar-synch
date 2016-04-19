662e894bc5b599d1ff48d6068eaea3729b71cf77
Emil Axelsson <emax@chalmers.se>
2016-04-19 17:25:02 +0200

    Fix capture example

--- examples/Capture.hs
+++ examples/Capture.hs
@@ -34,17 +34,11 @@ capture alsa capt play n
 
 captureMain :: Run ()
 captureMain = do
-    addInclude "\"feldspar_c99.h\""
-    addInclude "\"feldspar_array.h\""
-    addInclude "<math.h>"
     alsa@(ALSA {..}) <- importALSA
     capt <- newPCM
     play <- newPCM
     m    <- initPCM capt Capture  1 bufferLength periodLength
     n    <- initPCM play Playback 1 bufferLength periodLength
-    printf "%d %d\n" m n
-    let n = 128  -- TODO n must currently be known statically because otherwise
-                 --      we get an array declaration of unknown length.
     execSystem $ runSynch $ capture alsa capt play n
 
 runCapture = runCompiled'
